8367be2c8739f2578a2b3f30331f290952ffe571
Giovana-Arruda <giovanab.arruda@gmail.com>
2024-03-12 19:51:37 -0300

    refactor(pares.js): add a new console.log message

--- pares.js
+++ pares.js
@@ -4,7 +4,7 @@ const arrayNum = [2, 4, 3, 1, 6, 7, 9];
 
 arrayNum.forEach(num => {
     if (num % 2 === 0) {
-        console.log(num);
+        console.log("Os números pares do array são: ",num);
     }
 });
 
