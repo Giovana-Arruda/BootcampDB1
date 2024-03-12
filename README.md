34cf00ea16fbb564579f09941831fe0bf24ea7e0
Giovana-Arruda <giovanab.arruda@gmail.com>
2024-03-11 22:01:20 -0300

    criacao de um arquivo js

--- README.md
+++ README.md
@@ -6,5 +6,4 @@
 - Contexto Histórico;
 - Instalação e configuração inicial ;
 - Primeiros comandos apresentados;
-
-
+- Criação de um arquivo de código que retorna os números pares de um Array em JavaScript.
---
+++ index.js
@@ -0,0 +1,10 @@
+//Criar um arquivo que dado o array de valores [2, 4, 3, 1, 6, 7, 9], retorne quais são pares. Fazer em JavaScript
+
+const arrayNum = [2, 4, 3, 1, 6, 7, 9];
+
+arrayNum.forEach(num => {
+    if (num % 2 === 0) {
+        console.log(num);
+    }
+});
+
