541a9219e665f091c0bb2ac5580b48eef6d3e270
Giovana-Arruda <giovanab.arruda@gmail.com>
2024-03-18 20:32:54 -0300

    refactor

--- impares.js
+++ impares.js
@@ -1,7 +1,7 @@
-const arrayNum = [2, 4, 3, 1, 6, 7, 9, 8, 10, 0];
+const arrayNum = [2, 4, 3, 1, 6, 7, 9, 8, 10, 7, 5, 1];
 
 arrayNum.forEach(num => {
     if (num % 2 !== 0) {
-        console.log(num);
+        console.log("Esses são números impares: ", num);
     }
 });
