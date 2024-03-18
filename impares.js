24a773127f39acac3792bd9cae5da3ecd72f49a1
Giovana-Arruda <giovanab.arruda@gmail.com>
2024-03-18 19:48:18 -0300

    feat: criando soma array e editando arquivo maiores20 e impares

--- impares.js
+++ impares.js
@@ -1,7 +1,7 @@
-const arrayNum = [2, 4, 3, 1, 6, 7, 9];
+const arrayNum = [2, 4, 3, 1, 6, 7, 9, 8, 10, 0];
 
 arrayNum.forEach(num => {
     if (num % 2 !== 0) {
-        console.log(num);
+        console.log("Números ímpares: ", num);
     }
 });
